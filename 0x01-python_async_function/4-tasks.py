71e00f3bca3a6f2ce7831ddafd4b91369636a1be
Yanney677 <w.yanney1991@gmail.com>
2024-03-19 02:50:41 +0000

    Completed task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,15 +12,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns the generate wait_random n times"""
-    tasks = [] # Create tasks using task_wait_random
-    delays = [] #  Create a list to store task results
+    tasks = []
+    delays = []
 
     for i in range(n):
         task = task_wait_random(max_delay)
         tasks.append(task)
 
     for task in asyncio.as_completed((tasks)):
-        delay = await task # Await the task to get the result
+        delay = await task
         delays.append(delay)
 
-    return delays # Return the list of delays
+    return delays
