ec338971c2b9b3c8ac141bbf0ef2e8bb89dfc2eb
Yanney677 <w.yanney1991@gmail.com>
2024-03-20 00:16:02 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task's 0 Module
+"""
+
+import asyncio
+import random
+
+
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Iterate 10 times, 1 sec wait each time"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
